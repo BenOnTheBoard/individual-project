e183f442eb97e2b174dd12da984e73039320f696
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-12-01 09:16:04 +0000

    Fixes execution-log test

--- src/app/algorithm-page/sidebar/execution-log/execution-log.component.spec.ts
+++ src/app/algorithm-page/sidebar/execution-log/execution-log.component.spec.ts
@@ -6,20 +6,59 @@ import { ExecutionLogComponent } from './execution-log.component';
 describe('ExecutionLogComponent', () => {
   let component: ExecutionLogComponent;
   let fixture: ComponentFixture<ExecutionLogComponent>;
-  let playback: PlaybackService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      providers: [ PlaybackService, ExecutionLogComponent ]
-    })
-    .compileComponents();
+      imports: [ExecutionLogComponent],
+      providers: [
+        {
+          provide: PlaybackService,
+          useValue: {
+            commandList: [
+              {
+                lineNumber: 0,
+                freeAgents: [],
+                matches: new Map(),
+                stepVariables: {},
+                group1CurrentPreferences: new Map<string, string[]>([
+                  ["1", ["A", "B", "C"]],
+                  ["2", ["B", "A", "C"]],
+                  ["3", ["C", "A", "B"]],
+                ]),
+                group2CurrentPreferences: new Map<string, string[]>([
+                  ["A", ["1", "2", "3"]],
+                  ["B", ["2", "1", "3"]],
+                  ["C", ["3", "1", "2"]],
+                ]),
+                currentlySelectedAgents: [],
+                currentLines: [],
+                algorithmSpecificData: {},
+                relevantPreferences: [],
+              }
+            ],
+            algorithmData: {
+              numberOfGroup1Agents: 3,
+              numberOfGroup2Agents: 3,
+              currentAlgorithm: {
+                id: "smp-man-egs",
+                name: "Stable Marriage Problem",
+                orientation: ["Man", "Woman"],
+                equalGroups: true,
+              },
+              pluralMap: new Map([["Man", "Men"], ["Woman", "Women"],]),
+              descriptions: []
+            },
+            previousStepCounter: 0,
+            setAlgorithm: jasmine.createSpy('setAlgorithm')
+          }
+        },
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExecutionLogComponent);
     component = fixture.componentInstance;
-    playback = TestBed.inject(PlaybackService);
-    playback.algorithmData["description"] = ["a", "b", "c"];
     fixture.detectChanges();
   });
 
