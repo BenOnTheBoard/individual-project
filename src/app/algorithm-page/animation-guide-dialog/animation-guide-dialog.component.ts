424b264b807f0ceed45d841a4e0418755c10106a
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-12-01 08:10:04 +0000

    Fixes animation guide imports

--- src/app/algorithm-page/animation-guide-dialog/animation-guide-dialog.component.ts
+++ src/app/algorithm-page/animation-guide-dialog/animation-guide-dialog.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-animation-guide-dialog',
   templateUrl: './animation-guide-dialog.component.html',
-  styleUrls: ['./animation-guide-dialog.component.scss']
+  styleUrls: ['./animation-guide-dialog.component.scss'],
+  imports: [MatDialogModule, MatButtonModule]
 })
 export class AnimationGuideDialogComponent implements OnInit {
 
