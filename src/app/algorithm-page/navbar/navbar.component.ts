d45bad42b480ed0a670d21f4df003bfece05063b
Ben Whitehead <ben.pi.whitehead@gmail.com>
2026-01-29 17:00:45 +0000

    Sketches new navbar for algorithm page

---
+++ src/app/algorithm-page/navbar/navbar.component.ts
@@ -0,0 +1,89 @@
+import { Component, input, OnInit, output } from '@angular/core';
+import { PlaybackService } from '../services/playback/playback.service';
+import { AlgorithmRetrievalService } from 'src/app/algorithm-retrieval.service';
+import { CanvasService } from '../services/canvas/canvas.service';
+import { UtilsService } from 'src/app/utils/utils.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { AnimationGuideDialogComponent } from '../animation-guide-dialog/animation-guide-dialog.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'alg-page-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  imports: [MatIconModule, MatTooltip, BrowserModule],
+})
+export class NavbarComponent implements OnInit {
+  duringAnimation = input<boolean>();
+
+  showCode = false;
+  showInfo = false;
+  SRStable = true;
+  step = 0;
+
+  // for instructions to page
+  commandEmitter = output<string>();
+  // delivering info to page
+  dialogOpen = output<boolean>();
+  tutorialStep = output<number>();
+
+  constructor(
+    public playback: PlaybackService,
+    public algorithmService: AlgorithmRetrievalService,
+    public drawService: CanvasService,
+    public utils: UtilsService,
+    public dialog: MatDialog,
+    public router: Router,
+  ) {}
+
+  ngOnInit(): void {}
+
+  updateTutorialStep(step: number): void {
+    this.step = step;
+    this.tutorialStep.emit(this.step);
+  }
+
+  updateToNextTutorialStep() {
+    this.updateTutorialStep((this.step + 1) % 4);
+  }
+
+  toggleLeftSidebar(): void {
+    this.showCode = !this.showCode;
+    this.commandEmitter.emit('toggleLeftSidebar');
+  }
+
+  toggleRightSidebar(): void {
+    this.showInfo = !this.showInfo;
+    this.commandEmitter.emit('toggleLeftSidebar');
+  }
+
+  toggleSRStable(): void {
+    this.SRStable = !this.SRStable;
+    this.commandEmitter.emit('toggleSRStable');
+  }
+
+  emitGoHome(): void {
+    this.commandEmitter.emit('goHome');
+  }
+
+  emitGeneratePreferences(): void {
+    this.commandEmitter.emit('generatePreferences');
+  }
+
+  openAnimationGuideDialog(): void {
+    this.dialogOpen.emit(true);
+    const dialogRef = this.dialog.open(AnimationGuideDialogComponent);
+    dialogRef.afterClosed().subscribe(() => {
+      this.dialogOpen.emit(false);
+    });
+  }
+
+  refreshCanvas(): void {
+    this.drawService.alwaysShowPreferences =
+      !this.drawService.alwaysShowPreferences;
+    this.drawService.redrawCanvas();
+  }
+}
