0c0236c9dcb2e75971339b741d7fcbf510955d29
Ben Whitehead <ben.pi.whitehead@gmail.com>
2026-02-26 13:06:26 +0000

    Adds SMT Super Tests

--- src/app/algorithms/algorithm-services/smt-super/smt-super.service.spec.ts
+++ src/app/algorithms/algorithm-services/smt-super/smt-super.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SMTSuperService } from './smt-super.service';
 
-describe('EgsStableMarriageService', () => {
+const instanceCount = 600;
+
+describe('SMTSuperService', () => {
   let service: SMTSuperService;
 
   beforeEach(() => {
@@ -10,7 +11,13 @@ describe('EgsStableMarriageService', () => {
     service = TestBed.inject(SMTSuperService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeDefined();
+  it(`produces stable solutions x${instanceCount}`, () => {
+    let stable = true;
+    for (let i = 0; i < instanceCount; i++) {
+      const agentCount = Math.floor(Math.random() * (9 - 2) + 2);
+      service.run(agentCount);
+      if (!service.isStable()) stable = false;
+    }
+    expect(stable).toBeTrue();
   });
 });
